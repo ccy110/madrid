40ff4ea120ebc76caea11657b05bd417a4d4935c
cuichengyou <cuichengyou@fotoable.com>
2018-11-09 18:33:13 +0800

    test

---
+++ src/181009.erl
@@ -0,0 +1,22 @@
+%% about file
+
+%fun() -> ["path1","path2"]; ps: wildcard:通配符
+    filelib:wildcard("./deps/*/ebin")
+
+
+
+%% about erlang port
+
+%
+erlang:port_command()
+
+
+
+
+
+%% about code load
+
+
+
+
+
---
+++ src/c/inform.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+	printf("A.c is used to end final program. \n");
+	return 0;
+}
+
+
+
